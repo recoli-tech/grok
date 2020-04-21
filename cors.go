9864f7dfd4b12b1c891df3c20be2d631e0aaba42
Guilherme Firmiano <guilherme.firmiano@dafiti.com.br>
2020-04-21 19:16:55 -0300

    Cors add Access-Control-Allow-Credentials and origins

--- cors.go
+++ cors.go
@@ -9,9 +9,11 @@ import (
 // CORS ...
 func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		origin := c.GetHeader("Origin")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept, *")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(http.StatusOK)
